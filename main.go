3339cf5a87e05731dc2ae637e1324e92d69fd5e6
Victor-Choe <bigy1@naver.com>
2023-11-29 15:19:55 +0900

    채널 구조 제외

--- main.go
+++ main.go
@@ -19,7 +19,8 @@ func main() {
 		return
 	}
 	defer port.Close()
-	//예상 시나리오
+
+	// 예상 시나리오
 	expectedSequence := []string{
 		"On\r\n",
 		"Off\r\n",
@@ -30,22 +31,12 @@ func main() {
 		"Center\r\n",
 		"Stop\r\n",
 	}
-	//버퍼 채널
-	bufferChannel := make(chan string, 10)
 
 	successCounting := 0
-	failreCounting := 0
-	//차후 os.Args[1]로 받아올 예정
-	totalCounting := 10
-	//시도 횟수
-	counting := 0
+	failureCounting := 0
 
-	//종료 채널
-	//signalChannel := make(chan os.Signal, 1)
-	//signal.Notify(signalChannel, syscall.SIGINT, syscall.SIGTERM)
-
-	//토탈 카운팅만큼 반복
-	for i := 0; i < totalCounting; i++ {
+	// 토탈 카운팅만큼 반복
+	for counting := 0; counting < len(expectedSequence); counting++ { // 수정: totalCounting 대신 예상 시퀀스 길이 사용
 		buf := make([]byte, 128)
 		// 데이터 수신
 		n, err := port.Read(buf)
@@ -55,24 +46,23 @@ func main() {
 		}
 
 		// 수신한 데이터를 문자열로 변환
-		data := string(buf[:n])
-		counting++
-		//total
-		bufferChannel <- data
+		receivedData := string(buf[:n])
+
+		// 데이터 비교
+		if receivedData == expectedSequence[counting] {
+			successCounting++
+			fmt.Printf("Success: %v\n", successCounting)
+			fmt.Printf("Expected: %v\n", expectedSequence[counting])
+			fmt.Printf("Received: %v\n", receivedData)
+		} else {
+			failureCounting++
+			fmt.Printf("Failure: %v\n", failureCounting)
+			fmt.Printf("Expected: %v\n", expectedSequence[counting])
+			fmt.Printf("Received: %v\n", receivedData)
+		}
 	}
 
-	receivedData := <-bufferChannel
-
-	//데이터 비교
-	if receivedData == expectedSequence[counting%totalCounting] {
-		successCounting++
-		fmt.Printf("Success: %v\n", successCounting)
-		fmt.Printf("Expected: %v\n", expectedSequence[counting%totalCounting])
-		fmt.Printf("Received: %v\n", receivedData)
-	} else {
-		failreCounting++
-		fmt.Printf("Failure: %v\n", failreCounting)
-		fmt.Printf("Expected: %v\n", expectedSequence[counting%totalCounting])
-		fmt.Printf("Received: %v\n", receivedData)
-	}
+	// 결과 출력
+	fmt.Printf("성공 횟수: %v\n", successCounting)
+	fmt.Printf("실패 횟수: %v\n", failureCounting)
 }
